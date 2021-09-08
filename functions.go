ad4c2e7828f56eb45e11b5ad651bee9013189a92
Satishkumar Dhule <satishkumar.dhule@gmail.com>
2021-09-08 09:00:41 +0530

    pointer as function argument

--- functions.go
+++ functions.go
@@ -19,6 +19,16 @@ func doubleAt(array []int, ix int) {
 	array[ix] *= 2
 }
 
+func double(a int) {
+
+	a *= 2
+}
+
+func doublePtr(a *int) {
+
+	*a *= 2
+}
+
 func main() {
 
 	val := add(1, 2)
@@ -31,5 +41,13 @@ func main() {
 	doubleAt(a, 2)
 
 	fmt.Println(a)
+
+	i := 10
+
+	double(i)
+	fmt.Println(i) //no change in value as i awas passed by value
+
+	doublePtr(&i)
+	fmt.Println(i) //change in value as i was passed by ref
 
 }
