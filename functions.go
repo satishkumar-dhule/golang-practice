6758a2a6f264874553dd4151249e2e3bfcfd70ad
Satishkumar Dhule <satishkumar.dhule@gmail.com>
2021-09-08 08:54:38 +0530

    array as function param

--- functions.go
+++ functions.go
@@ -14,6 +14,11 @@ func devmod(a int, b int) (int, int) {
 	return a / b, a % b
 }
 
+func doubleAt(array []int, ix int) {
+
+	array[ix] *= 2
+}
+
 func main() {
 
 	val := add(1, 2)
@@ -21,5 +26,10 @@ func main() {
 
 	d, m := devmod(30, 4)
 	fmt.Println(d, m)
+
+	a := []int{1, 2, 3, 4, 5, 6}
+	doubleAt(a, 2)
+
+	fmt.Println(a)
 
 }
